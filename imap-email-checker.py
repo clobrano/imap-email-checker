7e9fc509c08c4aa43256852fc2f308d888f90531
Carlo Lobrano <carlo.lobrano@telit.com>
2017-05-26 17:34:33 +0200

    fixes: indentation, notification and logging

--- imap-email-checker.py
+++ imap-email-checker.py
@@ -51,68 +51,75 @@ debug = arguments['--debug']
 WAIT = 60 * min
 
 def signal(message):
-	if notify == 'on':
-		message += ' {0}'.format(datetime.now())
-		pynotify.init('PyEmail')
-		notification = pynotify.Notification('PyEmail', message)
-		notification.show()
+    if notify == 'on':
+        message += ' {0}'.format(format_time_hms())
+        pynotify.init('PyEmail')
+        notification = pynotify.Notification('PyEmail', message)
+        notification.show()
 
 
 def log(message):
-	if debug:
-		print(message)
+    if debug:
+        print('[{time}] {message}'.format(
+            time=format_time_hms(),
+            message=message))
 
 def error(message):
-	print('[Error]: {0}'.format(message))
+    print('[Error]: {0}'.format(message))
+
+
+def format_time_hms ():
+    time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    return time_str.split('.')[0]
 
 if __name__ == '__main__':
-	unseen = '0'
+    unseen = '0'
+    connected = False
 
-	connected = False
+    while True:
+        try:
+            M = imaplib.IMAP4_SSL(EMAIL_IMAP_SERVER)
 
-	while True:
-		try:
-			M = imaplib.IMAP4_SSL(EMAIL_IMAP_SERVER)
+            rv, data = M.login(EMAIL_ACCOUNT, password)
 
-			rv, data = M.login(EMAIL_ACCOUNT, password)
+            if 'OK' != rv:
+                log ('Could not connect to %s' % EMAIL_IMAP_SERVER)
+                break
 
-			if 'OK' != rv:
-				log ('Could not connect to %s' % EMAIL_IMAP_SERVER)
+            elif not connected:
+                connected = True
+                log ('Connected')
+                signal ('Connected')
 
-			elif not connected:
-				connected = True
-				log ('Connected')
-				signal ('Connected')
+            rv, data = M.select('INBOX')
+            if rv == 'OK':
+                rv, data = M.status('INBOX', '(UNSEEN)')
+                if rv == 'OK' and len(data) == 1:
+                    new_unseen = re.search('[0-9]+', data[0]).group(0)
+                    if new_unseen:
+                        log('{unseen} unseen e-mail(s), next check in {min} minutes.'.format(unseen=new_unseen, min=min))
+                    M.close()
 
-				rv, data = M.select('INBOX')
-				if rv == 'OK':
-					rv, data = M.status('INBOX', '(UNSEEN)')
-					if rv == 'OK' and len(data) == 1:
-						unseen = re.search('[0-9]+', data[0]).group(0)
-						log('[%s] %s unseen e-mail(s), next check in %d minutes.' %\
-								(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()),
-									unseen,
-									min))
-						M.close()
+            M.logout()
 
-				M.logout()
-
-				if unseen != '0':
-					signal('%s Unseen email(s)' % unseen)
+            if new_unseen != unseen:
+                signal('%s Unseen email(s)' % new_unseen)
+                unseen = new_unseen
+            else:
+                log ("Unread messages but already notified");
 
 
-		except imaplib.IMAP4.error as err:
-			msg = '%s for user "%s" and imap server "%s"' % (err, EMAIL_ACCOUNT, EMAIL_IMAP_SERVER)
-			error(msg)
-			signal(msg)
-			sys.exit(1)
+        except imaplib.IMAP4.error as err:
+            msg = '%s for user "%s" and imap server "%s"' % (err, EMAIL_ACCOUNT, EMAIL_IMAP_SERVER)
+            error(msg)
+            signal(msg)
+            sys.exit(1)
 
-		except Exception as e:
-			print(e)
-			signal('Error: {0}'.format(e.strerror))
-			if not 101 == e.errno:
-				sys.exit(1)
+        except Exception as e:
+            print(e)
+            signal('Error: {0}'.format(e.strerror))
+            if not 101 == e.errno:
+                    sys.exit(1)
+        time.sleep(WAIT)
 
-		time.sleep(WAIT)
-	
-	signal ('Exiting')
+    signal ('Exiting')
