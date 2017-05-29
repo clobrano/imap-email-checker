9113c0267793ae8c3649a1ff077f5b20d85483ef
Carlo Lobrano <carlo.lobrano@telit.com>
2017-05-29 11:12:20 +0200

    configuration: added json configuration file and multiple folders to check

--- imap-email-checker.py
+++ imap-email-checker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 '''
 Usage:
-	./imap-email-checker --email=ACCOUNT_ADDRESS --imap=IMAP_SERVER_ADDRESS [(--notify=on | --notify=off)] [--pass=PASSWORD] [--time=MIN] [--debug]
+	./imap-email-checker [--email=ACCOUNT_ADDRESS] [--imap=IMAP_SERVER_ADDRESS] [(--notify=on | --notify=off)] [--pass=PASSWORD] [--time=MIN] [--debug]
 
 Options:
 	-h --help	show this screen
@@ -21,30 +21,63 @@ import re		# regex library in order to extract unseen e-mails count
 import pynotify	# binding for libnotify
 import time
 from datetime import datetime
+import json
+import os
 
 # ==============================================
 # Get configuration values
 # ==============================================
+
+HOME = os.path.expanduser('~')
+CONFIG = os.path.join (HOME, '.imap-checker');
+
+try:
+    with open(CONFIG, 'r') as c:
+        configuration_str = c.read()
+    configuration = json.loads(configuration_str)
+except IOError as e:
+    configuration = {}
+    print ("Could not find configuration file: {}".format(e))
+
 arguments = docopt(__doc__, version='IMAP E-mail checker')
 
-EMAIL_ACCOUNT = arguments['--email']
-EMAIL_IMAP_SERVER = arguments['--imap']
-
-
-if arguments['--pass']:
-	password = arguments['--pass']
+if arguments['--email']:
+    EMAIL_ACCOUNT = arguments['--email']
+elif configuration['email']:
+    EMAIL_ACCOUNT = configuration['email']
 else:
-	password = getpass.getpass()
+    log ("No email account defined. Exiting...");
+    sys.exit(1)
+
+if arguments['--imap']:
+    IMAP_SERVER = arguments['--imap']
+elif configuration['server']:
+    IMAP_SERVER = configuration['server']
+else:
+    log ("No imap server defined. Exiting...");
+    sys.exit(1)
+
+# TODO better if we encrypt password, but at the moment you can provide it securely from command line with getpass
+if arguments['--pass']:
+    password = arguments['--pass']
+elif configuration['password']:
+    password = configuration['password']
+else:
+    password = getpass.getpass()
 
 if arguments['--time']:
-	min = eval(arguments['--time'])
+    min = eval(arguments['--time'])
+elif configuration['time']:
+    min = int(configuration['time'])
 else:
-	min = 10	# 10 minutes delay between e-mail checks (default)
+    min = 10	# 10 minutes delay between e-mail checks (default)
 
 if arguments['--notify']:
-	notify = arguments['--notify']
+    notify = arguments['--notify']
+elif configuration['notify']:
+    notify = configuration['notify']
 else:
-	notify = 'on'	# notification enable by default
+    notify = 'on'	# notification enable by default
 
 debug = arguments['--debug']
 
@@ -52,7 +85,6 @@ WAIT = 60 * min
 
 def signal(message):
     if notify == 'on':
-        message += ' {0}'.format(format_time_hms())
         pynotify.init('PyEmail')
         notification = pynotify.Notification('PyEmail', message)
         notification.show()
@@ -72,56 +104,68 @@ def format_time_hms ():
     time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
     return time_str.split('.')[0]
 
+
+def check_folder (connection, folder):
+    rv, data = connection.select(folder)
+    if rv == 'OK':
+        rv, data = connection.status(folder, '(UNSEEN)')
+        if rv == 'OK' and len(data) == 1:
+            return int(re.search('[0-9]+', data[0]).group(0))
+        connection.close()
+    return 0
+
+
 if __name__ == '__main__':
-    unseen = '0'
+    unseen = 0
     connected = False
 
     while True:
         try:
-            M = imaplib.IMAP4_SSL(EMAIL_IMAP_SERVER)
+            M = imaplib.IMAP4_SSL(IMAP_SERVER)
             rv, data = M.login(EMAIL_ACCOUNT, password)
 
             if 'OK' != rv:
-                log ('Could not connect to %s' % EMAIL_IMAP_SERVER)
+                log ('Could not connect to %s' % IMAP_SERVER)
                 break
 
             elif not connected:
                 connected = True
                 log ('Connected')
-                signal ('Connected')
 
-            rv, data = M.select('INBOX')
-            if rv == 'OK':
-                rv, data = M.status('INBOX', '(UNSEEN)')
-                if rv == 'OK' and len(data) == 1:
-                    new_unseen = re.search('[0-9]+', data[0]).group(0)
-                    if new_unseen:
-                        log('{unseen} unseen e-mail(s), next check in {min} minutes.'.format(unseen=new_unseen, min=min))
-                    M.close()
+            new_unseen = 0
+            updated_folders = ''
+
+            for folder in configuration['folders']:
+                folder_unseen = check_folder (M, folder)
+                log('{unseen} unseen e-mail(s) in {folder}, next check in {min} minutes.'\
+                    .format(unseen=folder_unseen, folder=folder, min=min))
+
+                if folder_unseen > 0:
+                    new_unseen += folder_unseen
+                    if updated_folders == '':
+                        updated_folders += folder
+                    else:
+                        updated_folders += ', ' + folder
 
             M.logout()
 
-            if new_unseen == '0':
+            if new_unseen == 0:
                 unseen = new_unseen
 
             if new_unseen != unseen:
-                signal('%s Unseen email(s)' % new_unseen)
+                signal('{num} Unseen email(s) in [{updated_folders}]'\
+                        .format(num=new_unseen, updated_folders=updated_folders))
                 unseen = new_unseen
             else:
                 log ("Unread messages but already notified");
 
 
         except imaplib.IMAP4.error as err:
-            msg = '%s for user "%s" and imap server "%s"' % (err, EMAIL_ACCOUNT, EMAIL_IMAP_SERVER)
+            msg = '%s for user "%s" and imap server "%s"' % (err, EMAIL_ACCOUNT, IMAP_SERVER)
             error(msg)
             signal(msg)
             sys.exit(1)
 
-        except Exception as e:
-            print(e)
-            signal('Error: {0}'.format(e.strerror))
-            if not 101 == e.errno:
-                    sys.exit(1)
         time.sleep(WAIT)
 
     signal ('Exiting')
