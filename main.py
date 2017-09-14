45a9d2e3500e7506616397327c25f1e69f25d10d
Carlo Lobrano <carlo.lobrano@telit.com>
2017-09-14 09:09:02 +0200

    Improved error management and debugging

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 # vi: set ft=python :
 '''
 Usage:
-    ./imap-email-checker [--config=PATH]
+    ./imap-email-checker [--config=PATH] [--verbose]
 
 Options:
     -h --help                   Show this screen
     --config=PATH               Path to configuration file [default: ~/.imap-checker]
+    -v, --verbose               Verbose mode for debugging purpose
 '''
 
 import sys
@@ -24,8 +25,7 @@ import gi
 gi.require_version('GLib', '2.0')
 from gi.repository import GLib
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(levelname)s %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(levelname)s %(message)s')
 LOG = logging.getLogger(__name__)
 
 
@@ -48,7 +48,7 @@ def get_configuration(filepath: str) -> str:
     "browser_url": ""
     "folders": ["INBOX", "Do not remove INBOX", "Another Folder"]
 }''')
-    sys.exit(1)
+        sys.exit(1)
 
     return configuration
 
@@ -60,8 +60,7 @@ def open_browser_cb(notification, action: str, url: str):
 
 
 def check_emails(account: dict) -> bool:
-    '''Check unseen emails in folder'''
-
+    '''Check unseen emails'''
     server = account['server']
     email = account['email']
     password = account['password']
@@ -70,19 +69,27 @@ def check_emails(account: dict) -> bool:
     conn = imaplib.IMAP4_SSL(server)
 
     ret, _ = conn.login(email, password)
+
     if ret != 'OK':
-        LOG.fatal('could not login to the e-mail: %s', ret)
+        LOG.fatal('could not log-in into account %s: %s', email, ret)
+
+        return GLib.SOURCE_REMOVE
 
     report = dict()
+
     for folder in folders:
         ret, _ = conn.select(folder)
+
         if ret != 'OK':
             LOG.error('could not select folder %s: %s', folder, ret)
+
             continue
 
         ret, data = conn.status(folder, '(UNSEEN)')
+
         if ret != 'OK':
             LOG.error('could not check folder %s: %s', folder, ret)
+
             continue
 
         conn.close()
@@ -94,10 +101,12 @@ def check_emails(account: dict) -> bool:
             report[folder] = unseen
 
     ret, _ = conn.logout()
+
     if ret != 'BYE':
         LOG.warning('could not logout: %s', ret)
 
     unseen = sum([unseen for unseen in report.values()])
+
     if unseen and unseen != account['unseen']:
         account['unseen'] = unseen
 
@@ -111,20 +120,30 @@ def check_emails(account: dict) -> bool:
                                     open_browser_cb, account['browser_url'])
             notification.show()
 
-    return True
+    return GLib.SOURCE_CONTINUE
 
 
 def main():
     '''Main function'''
     arguments = docopt(__doc__)
+
+    if arguments['--verbose']:
+        LOG.setLevel(logging.DEBUG)
+
     config_file = os.path.expanduser(arguments['--config'])
     account = get_configuration(config_file)
     account['unseen'] = 0
 
-    mainloop = GLib.MainLoop()
-    GLib.timeout_add_seconds(int(account['time']), check_emails, account)
-
     pynotify.init('PyEmail', 'glib')
+    check_emails(account)
+
+    mainloop = GLib.MainLoop()
+    event_id = GLib.timeout_add_seconds(
+        interval=int(account['time']), function=check_emails, data=account)
+
+    if not event_id:
+        LOG.fatal('could not setup e-mail checker function')
+        sys.exit(1)
 
     mainloop.run()
 
